90f6fe5c57c2bcf2897067e72f42decdb2f227fe
Rafael Lima <rafael.souza1207@gmail.com>
2023-09-21 21:19:18 -0300

    update async

---
+++ async_coletar_numeros.py
@@ -0,0 +1,114 @@
+import json
+import re
+import aiofiles
+from playwright.async_api import async_playwright
+from time import sleep
+from pathlib import Path
+import asyncio
+todas_cidades = []
+uf_estados = json.loads(open('uf_estados.json').read())
+acompanhantes = []
+
+
+async def quantidade_numeros():
+     async with aiofiles.open('telefones.txt', mode='r') as f:
+         content = await f.read()
+         return content
+
+
+async def filtrar_cidades_por_uf(uf):
+    
+         async with aiofiles.open(f"cidades/{uf_estados[uf]}",mode='r') as f:
+            content = await f.read()
+            link = json.loads(content)
+            #link = await json.load(f)
+            for url in link['links']:
+                if re.search('acompanhantes', url):
+                    todas_cidades.append(url)
+                    print(url)
+
+async def filtrar_tudo():
+    
+    for uf in uf_estados.keys():
+        await filtrar_cidades_por_uf(uf)
+        
+
+
+async def buscar_numeros():
+    # APAGARÁ O ARQUIVO TELEFONES SEMPRE QUE A FUNÇÃO FOR INVOCADA
+    path = Path('telefones.txt')
+    if path.exists():
+        path.unlink()
+
+    async with async_playwright() as p:
+        browser =  await p.chromium.launch(headless=False, args=['--no-sandbox', '--disable-setuid-sandbox'])
+        context =  await browser.new_context(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/000000000 Safari/537.36')
+        page = await context.new_page()
+        
+        for cidade in todas_cidades:
+            
+            await page.goto(cidade)
+            try:
+                botao = await page.wait_for_selector('xpath=/html/body/div[2]/div[1]/div/div/section/div/button', timeout=3000)
+                await botao.click()
+            except:
+                pass
+            inner_text = await page.inner_text('body')
+            if 'Ainda não há acompanhantes nesta cidade' in inner_text:
+                print('Não há acompanhantes nesta cidade')
+            else:
+                links = await page.query_selector_all('a')
+                    
+                for link in links:
+                        # Inicializa 'valido' como False a cada iteração
+                        valido = False
+                            
+                            # Validando Acompanhante para extrair contato
+                        href = await link.get_attribute("href")
+                        if href:
+                            valido = bool(re.search(r'-\d+$', href))
+
+                        
+                        if valido:
+                            print(f'Acompanhante encontrado: {href}')
+                            if href not in acompanhantes:
+                                acompanhantes.append(href)
+                for acompanhante in acompanhantes: 
+                    page2 = await context.new_page()
+                    await page2.goto(acompanhante)
+                    content_p2 = await page2.content()
+                    if 'Tenha acesso ao número de telefone desse e outros perfis no ' in content_p2:
+                        print('Numero Bloqueado')
+                        await page2.close()
+                        continue
+            
+                    else:
+                            
+                            telefone = re.findall(r'\(\d{2}\) \d{5}-\d{4}',await page2.content())
+                            telefone = ''.join(telefone)
+                            telefone = str(telefone)
+                            telefone = telefone.replace('(', '') 
+                            telefone = telefone.replace(')', '')
+                            telefone = telefone.replace('-', '')
+                            telefone = telefone.replace(' ', '')
+                            telefone = '+55' + telefone
+                            async with aiofiles.open('telefones.txt', mode='a') as f:
+                                await f.write(telefone[0:13] + '\n')
+                                
+                                #Numeros secundarios
+                               # if len(telefone) > 13:
+                                   # f.write(telefone[14:] + '\n')
+                                #print(telefone[14:])
+                                print(telefone[0:13])
+                            
+                         
+                            await page2.close()
+
+
+async def main():
+    await filtrar_tudo()
+    await buscar_numeros()
+
+if __name__ == '__main__':
+
+    asyncio.run(main())
--- bot_telegram.py
+++ bot_telegram.py
@@ -1,24 +1,26 @@
-from telebot import TeleBot
+import telebot
 from telebot import types
-from coletar_numeros import  quantidade_numeros, filtrar_cidades_por_uf, buscar_numeros, filtrar_tudo
+from async_coletar_numeros import  quantidade_numeros, filtrar_cidades_por_uf, buscar_numeros, filtrar_tudo
 from datetime import datetime
-
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 respostas = {}
-
+token = '6005438493:AAHXK_ssUTXDZ8e6N4dlp2Yk37l1VthRRCg'
 admins = ['rafaellima777']
-bot = TeleBot(token)
+bot = AsyncTeleBot(token)
+
 estado = None
 ADMIN = False
 DATA = None
 @bot.message_handler(commands=['start'])
 
-def start(message):
+async def start(message):
     global ADMIN
     for adm in admins:
         if message.chat.username == adm:
             ADMIN = True
     if ADMIN: 
-        bot.send_message(message.chat.id, '''
+        await bot.send_message(message.chat.id, '''
         Digite /coletar  para iniciar a coleta de números no no Fatal Model\n
 Digite /whatsapp Para autenticar o seu whatsapp ao servidor para disparar mensagens\n
 Digite /usuario para permitir que outro usuario use o bot\n
@@ -27,12 +29,12 @@ Digite /numeros para ver quantos números foram coletados\n
 *Nota:* A função /whatsapp está em desenvolvimento, mas você pode usar o App desktop para disparar mensagens               
                         ''')
     else:
-        bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
+       await bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
 
 
 
 @bot.message_handler(commands=['coletar'])        
-def coletar(message):
+async def coletar(message):
     global ADMIN
     global DATA
     menu = types.InlineKeyboardMarkup(row_width=4)
@@ -48,37 +50,37 @@ def coletar(message):
     menu.add(types.InlineKeyboardButton('SC', callback_data='sc'))
     menu.add(types.InlineKeyboardButton('Todos', callback_data='todos'))
     if ADMIN:
-        bot.send_message(message.chat.id, f'Ultima Coleta: {DATA}')
-        bot.send_message(message.chat.id, 'Me informe um estado:', reply_markup=menu)
+        await bot.send_message(message.chat.id, f'Ultima Coleta: {DATA}')
+        await bot.send_message(message.chat.id, 'Me informe um estado:', reply_markup=menu)
     else:
-        bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
+        await bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
 
 @bot.message_handler(commands=['usuario'])
-def usuario(message):
+async def usuario(message):
     if ADMIN:
-        bot.send_message(message.chat.id, 'Digite o nome de usuário que deseja dar acesso')
-        bot.register_next_step_handler(message, adicionar)
+        await bot.send_message(message.chat.id, 'Digite o nome de usuário que deseja dar acesso')
+        await bot.register_next_step_handler(message, adicionar)
     else:
-        bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
+        await bot.send_message(message.chat.id, 'Vocë não tem permissão para usar esse comando')
         
-def adicionar(message):
+async def adicionar(message):
     usuario = message.text
     admins.append(usuario)
-    bot.send_message(message.chat.id, 'Usuário adicionado com sucesso')
-    bot.send_message(message.chat.id, f'Usuarios com acesso: {",  ".join(admins)}')
+    await bot.send_message(message.chat.id, 'Usuário adicionado com sucesso')
+    await bot.send_message(message.chat.id, f'Usuarios com acesso: {",  ".join(admins)}')
 
 @bot.message_handler(commands=['numeros'])
-def numeros(message):
+async def numeros(message):
     if ADMIN:
         with open('telefones.txt', 'r') as f:
             numeros = len(f.readlines())
-            bot.send_message(message.chat.id, f'Estamos com: {numeros} numeros armazenados')
+            await bot.send_message(message.chat.id, f'Estamos com: {numeros} numeros armazenados')
     else:
-        bot.send_message(message.chat.id, 'Você não tem permissões para fazer isso')        
+        await bot.send_message(message.chat.id, 'Você não tem permissões para fazer isso')        
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def handle_query(call):
+async def handle_query(call):
     if call.data == "sp":
         bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de SP, isso vai demorar um tempo, mas eu aviso quando acabar")
         filtrar_cidades_por_uf('sp')
@@ -88,80 +90,80 @@ def handle_query(call):
         
     elif call.data == "rj":
         global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de RJ, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de RJ, isso vai demorar um tempo, mas eu aviso quando acabar")
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        filtrar_cidades_por_uf('rj')
-        buscar_numeros()
+        await filtrar_cidades_por_uf('rj')
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
 
     elif call.data == "mg":
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MG, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MG, isso vai demorar um tempo, mas eu aviso quando acabar")
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
         filtrar_cidades_por_uf('mg')
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
 
     elif call.data == "ba":
         #global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de BA, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('ba')
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de BA, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('ba')
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
 
     elif call.data == "pr":
         #global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de PR, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('pr')
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de PR, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('pr')
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
     
     elif call.data == "ms":
         #global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MS, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('ms')
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MS, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('ms')
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f'Fim da coleta: {final}')
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f'Fim da coleta: {final}')
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
     
     elif call.data == "es":
         #global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de ES, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('es')
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de ES, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('es')
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f'Fim da coleta: {final}')
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f'Fim da coleta: {final}')
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
         
     
     elif call.data == 'sc':
         #global DATA 
         data_coleta = datetime.now()
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de SC, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('sc')
-        buscar_numeros()
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de SC, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('sc')
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
     
     elif call.data == 'go':
         bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de GO, isso vai demorar um tempo, mas eu aviso quando acabar")
@@ -173,28 +175,25 @@ def handle_query(call):
     elif call.data == 'mt':
         #global DATA
         data_coleta = datetime.now()
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MT, isso vai demorar um tempo, mas eu aviso quando acabar")
-        filtrar_cidades_por_uf('mt')
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de MT, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await filtrar_cidades_por_uf('mt')
         DATA = data_coleta.strftime("%d/%m/%Y - %H:%M:%S")
-        buscar_numeros()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
         
     elif call.data == 'todos':
         #global DATA
-        bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de SP, isso vai demorar um tempo, mas eu aviso quando acabar")
+        await bot.send_message(call.message.chat.id, "Ok, Vamos coletar todos os numeros do estado de SP, isso vai demorar um tempo, mas eu aviso quando acabar")
         DATA = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        filtrar_tudo()
-        buscar_numeros()
+        await filtrar_tudo()
+        await buscar_numeros()
         final = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-        bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
-        bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
+        await bot.send_message(call.message.chat.id, f"Fim da coleta: {final}")
+        await bot.send_message(call.message.chat.id, f"Coletamos {quantidade_numeros()} números")
 
-
-
-
-
-
-
-bot.polling()
+async def main():
+   await bot.polling()
+if __name__ == '__main__':
+    asyncio.run(main())
